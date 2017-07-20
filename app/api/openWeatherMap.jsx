cc9b2a8601a4b25fea0c692747f729b2d9d22fbc
Mario Adler-Kuczera <kontakt@silu-berlin.de>
2017-07-20 22:46:49 +0200

    added enhanced error handling

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -13,8 +13,8 @@ module.exports = {
             } else {
                 return res.data.main.temp
             }
-        }, function (res) { //error
-            throw new Error(res.data.message);
+        }, function (err) { //error
+            throw new Error('Unable to fetch weather for that location.');
         });
     }
 }
